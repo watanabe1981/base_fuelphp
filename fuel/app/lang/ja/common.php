80ed44304861d2aeea93d534117123e21d6a72d2
渡邉勇 <i.watanabe@funaffect.jp>
2017-01-16 16:14:15 +0900

    [add] デフォルト設定追加

---
+++ fuel/app/lang/ja/common.php
@@ -0,0 +1,54 @@
+<?php
+return [
+    // 管理画面系
+    'login_valid_error' => '入力エラー',
+    'access_deny'       => 'アクセス権限がありません',
+
+    'create_success'    => '作成しました。',
+    'create_error'      => '作成できませんでした。',
+
+    'update_success'    => '更新しました。',
+    'update_error'      => '更新できませんでした。',
+
+    'delete_success'    => '削除しました。',
+    'delete_error'      => '削除できませんでした。',
+
+    'sendmail_success'  => 'メール送信しました。',
+    'sendmail_error'    => 'メール送信しました。',
+
+    // ログイン
+    'login_fail'        => 'ログインID またはパスワードが正しくありません',
+    'deleted_user'      => 'ユーザーが削除されています',
+
+    // 一般共通
+    'please_select'    => '選択してください',
+
+    'username'          => 'ログインID',
+    'password'          => 'パスワード',
+    'password_confirm'  => 'パスワード（確認）',
+    'current_password'  => '現在のパスワード',
+    'new_password'      => '新しいパスワード',
+    'confirm_password'  => '新しいパスワード（確認）',
+
+    'id'                => 'ID',
+    'group'             => '権限',
+
+    'email'             => 'メールアドレス',
+    'phone'             => '電話番号',
+    'mobile'            => '携帯電話',
+    'fax'               => 'FAX番号',
+    'phone1'            => '電話番号 1',
+    'phone2'            => '電話番号 2',
+    'phone3'            => '電話番号 3',
+
+    'zipcode'           => '郵便番号',
+    'prefecture'        => '都道府県',
+    'address1'          => '市区町村',
+    'address2'          => '番地',
+    'building'          => '建物名など',
+
+    'remarks'           => '備考',
+
+    'unregistered'      => '未登録',
+
+];
